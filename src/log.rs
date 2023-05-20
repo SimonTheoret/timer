7f0afb0ee5821c1cb77571232f241c3cf752c563
Simon Theoret <simonteoret@hotmail.com>
2023-05-19 20:33:27 -0400

    Init

---
+++ src/log.rs
@@ -0,0 +1,5 @@
+
+struct Log {
+    path: String,
+    timer: &Timer,
+}
---
+++ src/timer.rs
@@ -0,0 +1,12 @@
+extern crate timer;
+
+struct CountdownTimer {
+    duration: u32,
+    timer: timer::Timer,
+}
+
+impl CountdownTimer {
+    fn start(&self){
+        pass
+    }
+}
