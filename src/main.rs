a4735b6c21c579059945f550d6927f346358b8d1
Simon Théorêt <simonteoret@hotmail.com>
2023-06-10 00:39:00 -0400

    Display A-OK

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
-// const SEC_IN_MINUTE: f32 = 60.;
+const SEC_IN_MINUTE: f32 = 60.;
 const MAX_MINUTES: f32 = 120.;
 
 fn main() -> Result<(), eframe::Error> {
@@ -17,21 +17,25 @@ fn main() -> Result<(), eframe::Error> {
 }
 struct Timer {
     duration: Arc<Mutex<f32>>,
-    widget_visible: bool,
+    widget_visible: Arc<Mutex<bool>>,
+    init_duration: f32,
 }
 
 impl Timer {
     fn new(duration: f32) -> Self {
         Timer {
             duration: Arc::new(Mutex::new(duration)),
-            widget_visible: true,
+            widget_visible: Arc::new(Mutex::new(true)),
+            init_duration: duration,
         }
     }
     fn start(&mut self, ctx: &egui::Context) {
+        self.init_duration = self.duration.lock().unwrap().clone();
         let duration_clone = Arc::clone(&self.duration);
         let context = ctx.clone();
         self.set_widget_state_false();
-        thread::spawn(move || {
+        context.request_repaint();
+        let handle = thread::spawn(move || {
             while *duration_clone.lock().unwrap() > 0. {
                 {
                     *duration_clone.lock().unwrap() -= 1. / 60.; // lock inside its own scope as to
@@ -41,36 +45,40 @@ impl Timer {
                 thread::sleep(Duration::new(1, 0));
             }
         });
-        self.set_widget_state_true()
-    }
-    fn widget_state(&self) -> bool {
-        match self.widget_visible {
-            true => true,
-            false => false,
+        while handle.is_finished() {
+            self.set_widget_state_true()
         }
     }
+    fn widget_state(&self) -> bool {
+        *self.widget_visible.lock().unwrap()
+    }
     fn set_widget_state_true(&mut self) {
-        self.widget_visible = true
+        *self.widget_visible.lock().unwrap() = true
     }
     fn set_widget_state_false(&mut self) {
-        self.widget_visible = false
+        *self.widget_visible.lock().unwrap() = false
     }
 }
 
 impl eframe::App for Timer {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Pomodoro");
-            ui.add_visible(
-                !self.widget_state(),
-                egui::widgets::ProgressBar::new(*self.duration.lock().unwrap()),
+            let timeleft = self.duration.lock().unwrap().clone();
+            let minutes = timeleft.clone().floor();
+            let seconds = (timeleft.clone().fract() * SEC_IN_MINUTE).floor(); //TODO:
+            // Make it better.
+            let progress_bar = egui::widgets::ProgressBar::new(
+                *self.duration.lock().unwrap() / self.init_duration,
             );
+            let progress_bar_textual = progress_bar.text(format!("{minutes} : {seconds}"));
+            ui.heading("Pomodoro");
+            ui.add_visible(!self.widget_state(), progress_bar_textual);
             ui.add_visible(
                 self.widget_state(),
                 egui::Slider::new(&mut *self.duration.lock().unwrap(), 0.0..=MAX_MINUTES)
                     .text("Duration"),
             );
-            ui.add_enabled_ui(self.widget_state(), |ui| {
+            ui.add_visible_ui(self.widget_state(), |ui| {
                 if ui.button("+").clicked() {
                     *self.duration.lock().unwrap() += 1.;
                 }
